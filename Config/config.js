ced7fdd0b37bc098d55966c6d5afd0ccf6418230
Boluwatife Moronkeji <moronkejiboluwatife8@gmail.com>
2022-11-07 13:06:24 +0100

    add mongodb atlas url

--- Config/config.js
+++ Config/config.js
@@ -4,6 +4,4 @@ require("dotenv").config()
 module.exports = {
     PORT: process.env.PORT || 3335,
     MONGODB_URL: process.env.MONGODB_URL,
-    // TEST_PORT: 4242,
-    // TEST_DB: mongodb://localhost:27017
 }
